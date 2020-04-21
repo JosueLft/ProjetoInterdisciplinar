d7e8b07b1d844aa3cef8cd26afcc733936949cc4
JosueFlx <josueflx1999@gmail.com>
2020-04-21 17:40:07 -0300

    --- Projeto Interdisciplinar ---
    	* Criado menu de exibição inicial
    	* Criado função de escolha das opções do menu
    	* Criado classe Cadastro.py

--- src/Main.py
+++ src/Main.py
@@ -4,18 +4,35 @@
 c = "Cadastro"
 v = "Visualizar"
 s = "Sair"
+escolha = 0
 
 # função
 def menu():
-    print("==========================")
-    print("== Lofty Papelaria Menu ==")
-    print("==========================\n")
+    print("=========================")
+    print("== Loft Papelaria Menu ==")
+    print("=========================")
+    print("  ---------------------")
+    print("  ||%12s(1)  ||" % c)
+    print("  ||%13s(2) ||" % v)
+    print("  ||%10s(3)    ||" % s)
+    print("  ---------------------")
 
-    print("--------------------------")
-    print("%s(1)" % c)
-    print("%s(2)" % v)
-    print("%s(3)" % s)
-    print("--------------------------")
+def choosen():
+    escolha = int(input("Informe o indice de sua escolha: "))
+    while True:
+        if(escolha == 1):
+            print("\nCarregando arquivos de cadastro!")
+            break
+        elif(escolha == 2):
+            print("\nCarregando arquivos de exibição!")
+            break
+        elif(escolha == 3):
+            print("\nAté Logo!")
+            break
+        else:
+            print("Opção invalida!\nPor favor informar um opção valida de acordo com os indices!")
+            escolha = int(input("Informe o indice de sua escolha: "))
 
 if __name__ == '__main__':
     menu()
+    choosen()
