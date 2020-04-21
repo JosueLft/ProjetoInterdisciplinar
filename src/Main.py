9bcc6469d1f07273c76dc7d9b4554d3d1ada6e8e
JosueFlx <josueflx1999@gmail.com>
2020-04-21 17:04:31 -0300

    --- Projeto Interdisciplinar ---
    	* Remoção da classe Artista.
    	* Alteração da classe main

--- src/Main.py
+++ src/Main.py
@@ -1,4 +1,21 @@
 # -*- uncoding: utf-8 -*-
 
+# variaveis
+c = "Cadastro"
+v = "Visualizar"
+s = "Sair"
+
+# função
+def menu():
+    print("==========================")
+    print("== Lofty Papelaria Menu ==")
+    print("==========================\n")
+
+    print("--------------------------")
+    print("%s(1)" % c)
+    print("%s(2)" % v)
+    print("%s(3)" % s)
+    print("--------------------------")
+
 if __name__ == '__main__':
-    
+    menu()
